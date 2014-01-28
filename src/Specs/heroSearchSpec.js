50e9232f1715369e753dbf1878b7940e880038d9
jmaxxz <jmaxxz@users.noreply.github.com>
2014-01-27 19:55:03 -0600

    Create simple hero search

--- src/Specs/heroSearchSpec.js
+++ src/Specs/heroSearchSpec.js
@@ -1,6 +1,41 @@
 describe('heroSearch', function() {
+  var oldHeros;
+  beforeEach(function(){
+    oldHeros = window.dota.heros;
+    window.dota.heros = {'Drow Ranger':{name:'Drow Ranger'}, 'Axe':{name:'Axe'}, 'Windranger':{name:'Windranger'}};
+  });
+
+  afterEach(function(){
+    window.dota.heros = oldHeros;
+  });
+
   var hs = window.dota.heroSearch;
-  it("has a match function", function() {
-    expect(typeof(hs.match)).toEqual('function');
+  describe('.match', function(){
+    var match = hs.match;
+
+    it('is a function', function() {
+      expect(typeof(match)).toEqual('function');
+    });
+
+    it('returns an array', function(){
+      var result = match();
+      expect(typeof(result)).toBe('object');
+      expect(result.length).toBeDefined();
+    });
+
+    it('returns all hero\'s who\'s names contain the query', function (){
+      var result = _.map(match('anger'),(function(v){return v.name;}));
+      expect(_.contains(result, 'Drow Ranger')).toBe(true);
+      expect(_.contains(result, 'Windranger')).toBe(true);
+      expect(result.length).toBe(2);
+    });
+
+    it('treats no search term as an empty string', function (){
+      expect(match()).toEqual(match(''));
+    });
+    it('"" returns all heros', function() {
+      var result = match('');
+      expect(result.length).toBe(3);
+    });
   });
 });
--- src/modules/heroSearch.js
+++ src/modules/heroSearch.js
@@ -1,8 +1,21 @@
+//require underscore
+
+window = window || {};
 window.dota =  window.dota || {};
 window.dota.heroSearch = (function (){
 
-  function match() {
+  function match(query) {
+    var heros = window.dota.heros;
+    query = query || '';
+    var result = [];
 
+    for(var name in heros){
+      if(name.indexOf(query) >= 0){
+        result.push(heros[name]);
+      }
+    }
+
+    return result;
   }
 
   return {
